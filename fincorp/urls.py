66e96c2873d81b673797fd358ecf1054d2d768f7
ABISHEKRS06 <rsakthedon@gmail.com>
2025-11-23 19:04:01 +0530

    Add Google site verification file

--- fincorp/urls.py
+++ fincorp/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('googled04731b798f8126a.html', TemplateView.as_view(template_name='googled04731b798f8126a.html')),
     path('', include('crm.urls')),
 ]
